dfacd627d64396a4cd03472e7553a4bedbb59388
league <league@imac-109.attlocal.net>
2022-10-12 18:29:20 -0700

    gggg

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,57 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
+		String[] array = {"one ", "two", "three", "four,", "five"};
 		
 		//2. print the third element in the array
-		
+		System.out.println(array[2]);
 		//3. set the third element to a different value
-		
+		array[2]= "what number are you?";
 		//4. print the third element again
-		
+		System.out.println(array[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for(int i=0;i<array.length;i++) {
+			System.out.println(array[i]);
+			
+		}
 		
 		//6. make an array of 50 integers
-		
+         int[] age = new int[50];
 		//7. use a for loop to make every value of the integer array a random number
 		
+		for (int i=0;i<age.length;i++) {
+			int randy = new Random().nextInt(300);
+			age[i]=randy;
+		}
+		//7. use a for loop to make every value of the integer array a random number
+		for(int h =0; h<50;h++) {
+	 for(int i = 0;i<age.length-1;i++) {
+		 if(age[i]>age[i+1]) {
+			 int temp = age[i+1];
+			 age[i+1]=age[i];
+			 
+			 
+		 }
+	 
+						
+						
+	 }
+		}
+		System.out.println(0);
 		//8. without printing the entire array, print only the smallest number in the array
-		
+		for (int i=0;i<age.length;i++) {
+		     System.out.println(age[i]);
+		}
+		//10. print the largest number in the array.
+		System.out.println(age[49]);
+	}
 		//9 print the entire array to see if step 8 was correct
 		
 		//10. print the largest number in the array.
-	}
+	
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,22 +1,76 @@
 package arrays;
 
+ 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
+	private static int i;
+
 	//1. make a main method
+public static void main(String[] args) {
+	
+	JFrame frame = new JFrame();
+   JPanel panel = new JPanel();
+   frame.add(panel);
+	
+	
 
 	//2. create an array of 5 robots.
-
+Robot[] rob =  new Robot[6];
 	//3. use a for loop to initialize the robots.
-
+for(int i = 0; i<rob.length;i++) {
+	System.out.println("it worked");
+	rob[i] = new Robot();
+	
+	
+	
+	
+}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
-
+for(int i = 0; i<rob.length;i++)
+{
+	rob[i].setX((i*120)+50);
+	rob[i].setY(500);
+}
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
-
+while(true) {
+	
+	for(int i = 0;i<rob.length;i++) {
+		if(rob[i].getY()>1){
+			int randy = new Random().nextInt(50);
+			rob[i].setSpeed(250);
+			rob[i].move(randy);
+	  try {
+		  
+		  Thread.sleep(200);
+	  } catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		
+	  }
+		}
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+}	
+else if(rob[i].getY()<=1) {
+	
+	int b= i+1;
+		JOptionPane.showMessageDialog(null,"Robot "+b+" wins!");
+		System.exit(0);
+	
+		}
 
-	//7. declare that robot the winner and throw it a party!
+}
 
-	//8. try different races with different amounts of robots.
+
+
+
+}	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
 }
